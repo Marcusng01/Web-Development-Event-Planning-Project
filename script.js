d83bf918a5f77cc2917f67f8813c36920a802a9b
Ng Wai Kit <marcus.ngwaikit01@gmail.com>
2023-06-08 08:05:07 +0800

    translucent header on scroll

--- script.js
+++ script.js
@@ -26,4 +26,11 @@ dropdownButton.addEventListener('mouseleave', contract);
 
 dropdownContainer.addEventListener('mouseleave', contract);
 
-
+//Header opacity change on scroll logic
+const header = document.querySelector('.header');
+window.addEventListener('scroll', (e)=>{
+    if(window.pageYOffset==0)
+        header.classList.remove("translucent");
+    else
+        header.classList.add("translucent");
+})

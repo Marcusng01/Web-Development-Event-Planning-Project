67bbde6e0c93df564f476ed56164f31c8a0cdc85
Ng Wai Kit <marcus.ngwaikit01@gmail.com>
2023-06-08 20:48:08 +0800

    bufiix
    Fix video not loading on page refresh
    Fix problem where carousel items is oddly position when window is resized

--- Title Page/titlePage.js
+++ Title Page/titlePage.js
@@ -8,22 +8,37 @@ function slideCarousel() {
   carouselItemWidth = carousel.querySelector('.carousel-item').offsetWidth;
   carouselItems.style.transform = `translateX(-${currentIndex * carouselItemWidth}px)`;
 }
-
+ 
 function showNextItem() {
-    currentIndex++;
-    if (currentIndex >= carouselItems.children.length) {
-    currentIndex = 0;
-    }
-    slideCarousel();
+  if(!carouselItems.classList.contains("transition"))
+    carouselItems.classList.add("transition");
+  currentIndex++;
+  if (currentIndex >= carouselItems.children.length) {
+  currentIndex = 0;
+  }
+  slideCarousel();
 }
 
 function showPreviousItem() {
-    currentIndex--;
-    if (currentIndex < 0) {
-    currentIndex = carouselItems.children.length - 1;
-    }
-    slideCarousel();
+  if(!carouselItems.classList.contains("transition"))
+    carouselItems.classList.add("transition");
+  currentIndex--;
+  if (currentIndex < 0) {
+  currentIndex = carouselItems.children.length - 1;
+  }
+  slideCarousel();
 }
+
 // Add event listeners to navigation buttons or any other triggers
 document.querySelector("#next-button").addEventListener('click', showNextItem);
 document.querySelector("#previous-button").addEventListener('click', showPreviousItem);
+
+
+function recalibrateCarousel() {
+  if(carouselItems.classList.contains("transition"))
+    carouselItems.classList.remove("transition");
+  slideCarousel();
+}
+
+// Recalibrate the carousel when changing window size
+window.addEventListener('resize',recalibrateCarousel, true);
